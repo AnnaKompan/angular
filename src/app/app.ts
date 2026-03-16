2185c7556f9655e8d23c4fca59f6e262ab2e509f
Anna Kompan <annakompanch@gmail.com>
2026-03-16 14:28:21 +0200

    @angular/material styles + routers

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,37 @@
 import { Component, signal } from '@angular/core';
-import { Test } from './components/test/test';
+// import { Test } from './components/test/test';
+import { RouterLinkActive, RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [Test],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    // RouterLinkActive,
+    CommonModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatTableModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
